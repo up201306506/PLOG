26c3f4be41bccb8bb09c7b649fc140aad23b21cb
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-10-27 11:18:42 +0000

    Dar_Baralho Update

--- Code/Dominup Código.pl
+++ Code/Dominup Código.pl
@@ -1,4 +1,6 @@
 ﻿
+:- use_module(library(random)).
+
 %%%%%%%%%%%%%%%%%%%%%%
 %%	tabuleiro		%%
 %%%%%%%%%%%%%%%%%%%%%%
@@ -58,7 +60,25 @@ jogador(b).
 :- dynamic mao/2.
 
 mao(a, []).
+mao(b, []).
 mostra_mao_exemplo :- mao(J, L), write('Mao do Jogador '), write(J), jogador(J), write(':'), nl, mostra_linha(L).
+
+% V - Mao Velha
+% N - Mao Nova
+% J - Jogador
+% P - Peça
+acrecentar_peca_mao(P, J) :- 	mao(J, V),
+								append(P,V,N),
+								retract(mao(J,V)),
+								assert(mao(J, N)).
+										
+						
+mudar_mao :-	mostra_mao_exemplo,
+				retract(mao(J, T)),
+				nl,
+				assert(mao(a, [ [2|1],[3|4],[5|5],[6|6],[8|1] ])),
+				mostra_mao_exemplo.				
+							
 
 
 %%%%%%%%%%%%%%%%%%%%%%
@@ -74,40 +94,50 @@ estado(T, J) :- tabuleiro(T), jogador(J).
 %%	Main			%%
 %%%%%%%%%%%%%%%%%%%%%%
 
+jogar :- 	repeat,
+			dar_baralho,
+			!.
+
 
 
 :- dynamic baralho/1.
 baralho([ [2|2], [2|1], [1|1], [3|2] ]).
 
-%baralho_vazio :- 	baralho(B),
-%					mostra_linha(B),
-%					!,
-%					length(B,0).
+baralho_vazio :- 	baralho(B),
+					mostra_linha(B),
+					!,
+					length(B,0).
 
+					
+element_at(X,[X|_],1).
+element_at(X,[_|L],K) :- element_at(X,L,K1), K is K1 + 1.
 
+get_peca_from_baralho(P) :- baralho(B),	
+							length(B, L),
+							LS is L + 1,
+							random(0, LS, X),
+							element_at(P, B, X).
+							
 first(F, [F|_]).
+
+
 delete_one(X,L1,L2) :- 	append(A1,[X|A2],L1),
 						append(A1,A2,L2).
 
 dar_baralho :- 	repeat,
 				baralho(B),
-				mostra_linha(B),
-				nl,
-				first(F, B),
-				delete_one(F,B,C),
+				get_peca_from_baralho(P),
+				delete_one(P,B,C),
 				retract(baralho(B)),
 				assert(baralho(C)),
+				acrecentar_peca_mao([P],a),
 				baralho(Y),
 				length(Y, 0).
 				
 				
 
 
-mudar_mao :-	mostra_mao_exemplo,
-		retract(mao(J, T)),
-		nl,
-		assert(mao(a, [ [2|1],[3|4],[5|5],[6|6],[8|1] ])),
-		mostra_mao_exemplo.
+
 		
 		
 		
