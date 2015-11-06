42fda2f4c971ca7bd50de05c9c856d474617e429
Mellus <dorianMahlerian@gmail.com>
2015-11-06 16:48:54 +0000

    Tabuleiro já é completamente dinâmico :dancers:

--- Code/auxiliar.pl
+++ Code/auxiliar.pl
@@ -7,7 +7,6 @@ list_element_at(X,[_|L],K) :-
 	list_element_at(X,L,K1), 
 	K is K1 + 1.
 			
-
 list_delete_one(X,L1,L2) :- 	
 	append(A1,[X|A2],L1),
 	append(A1,A2,L2).
@@ -55,6 +54,40 @@ tabuleiro_insere_linha_inicio :-
 	retract(tabuleiro(T)),
 	assert(tabuleiro(TF)).
 
+tabuleiro_insere_coluna_fim :-
+	tabuleiro(T),
+	length(T, Max),
+	!,
+	acrescenta_coluna_fim(T, 1, Max).
+
+acrescenta_coluna_fim(_, N, Max) :-
+	N > Max.
+acrescenta_coluna_fim(T, N, Max) :-
+	list_element_at(L, T, N),
+	append(L, [[0|0]], LF),
+	select(L, T, LF, TF),
+	retract(tabuleiro(T)),
+	assert(tabuleiro(TF)),
+	N1 is N+1,
+	acrescenta_coluna_fim(TF, N1, Max).
+	
+tabuleiro_insere_coluna_inicio :-
+	tabuleiro(T),
+	length(T, Max),
+	!,
+	acrescenta_coluna_inicio(T, 1, Max).
+
+acrescenta_coluna_inicio(_, N, Max) :-
+	N > Max.
+acrescenta_coluna_inicio(T, N, Max) :-
+	list_element_at(L, T, N),
+	append([[0|0]], L, LF),
+	select(L, T, LF, TF),
+	retract(tabuleiro(T)),
+	assert(tabuleiro(TF)),
+	N1 is N+1,
+	acrescenta_coluna_inicio(TF, N1, Max).
+	
 acrescenta_vazio(L, 1, LF) :-
 	append([[0|0]], L, LF).
 
@@ -69,6 +102,11 @@ acrescenta_vazio(L, N, LF) :-
 
 readInt(Texto, I, Min, Max) :-
 	repeat,
+	write(Texto), nl,
+	read(I),
+	I >= Min, I =< Max.
+
+readInt_NoRepeat(Texto, I, Min, Max) :-
 	write(Texto), nl,
 	read(I),
 	I >= Min, I =< Max.
