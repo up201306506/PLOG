d72bdf3318f87ae0493ba2a8fcdffbde843797be
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-11-08 17:51:15 +0000

    Documentação Auxiliar.pl :page_with_curl:

--- Code/auxiliar.pl
+++ Code/auxiliar.pl
@@ -1,7 +1,21 @@
 %%%%%%%%%%%%%%%%%%%%%%
 %% Manipular Listas	%%
 %%%%%%%%%%%%%%%%%%%%%%
-				
+	%	Funções Importantes:
+	%
+	%	:-list_element_at(E,L,N).
+	%		Devolve o N-ésimo elemento da lista L em E.
+	%
+	%	:-list_delete_one(X,L1,L2).
+	%		Remove o X-ésimo elemento da lista L1, resultando na lista L2.
+	%
+	%	:-matrix_setCell(L, C, TI, E, TF).
+	%		Recebe uma matriz TI (com o propósito de receber tabuleiros), e uma coordenada coluna C + linha L, e 
+	%		devolve a matriz TF igual à inicial excepto que o elemento na coordenada é trocado pelo elemento E.
+	%
+	%
+	
+	
 list_element_at(X,[X|_],1).
 list_element_at(X,[_|L],K) :- 
 	list_element_at(X,L,K1), 
@@ -22,6 +36,27 @@ matrix_setCellCol(N, [H|T], Piece, [H|T1]):-
 	Prev is N-1,
 	matrix_setCellCol(Prev, T, Piece, T1).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Redimensionar o Tabuleiro	%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%	Funções Importantes:
+	%
+	%	:-tabuleiro_dimensiona.
+	%		Encontra o tabuleiro actualmente definido pelo jogo e, com o uso às funções
+	%			*tabuleiro_primeira_linha_vazia.		*tabuleiro_insere_linha_inicio.
+	%			*tabuleiro_ultima_linha_vazia.			*tabuleiro_insere_linha_fim.
+	%			*tabuleiro_primeira_coluna_vazia.		*tabuleiro_insere_coluna_inicio.
+	%			*tabuleiro_ultima_coluna_vazia.			*tabuleiro_insere_coluna_fim.
+	%		identifica se é necessário expandir o tamanho deste tabuleiro para permitir os jogadores pousarem
+	%		peças na direcção ocupada. Um espaço vazio é o elemento [0|0].
+	%		Cada chamada acrescenta duas linhas ou duas colunas à direcção em necessidade. 
+	%		Não faz nada noutros casos.
+	%		É necessário chamar a função duas vezes para garantir que peças pousadas nos cantos do tabuleiro 
+	%		são lidadas adequadamente.
+	%
+	
+	
 tabuleiro_dimensiona :-
 	tabuleiro(T),
 	length(T, N),
@@ -138,6 +173,16 @@ acrescenta_vazio(L, N, LF) :-
 %%%%%%%%%%%%%%%%%%%%%%
 %% Outras funções	%%
 %%%%%%%%%%%%%%%%%%%%%%
+	%	Funções Importantes:
+	%
+	%	:-readInt(Texto, I, Min, Max).
+	%		Escreve um texto dado e pede ao jogadorum numero I que tem de se encontrar entre os 
+	%		valores Min e Max, senão pede um novo.
+	%
+	%	:-incrementador(P, N, Max).
+	%		Devolve em N, um a um, todos os valores entre P e Max, e depois falha.
+	%
+	
 
 readInt(Texto, I, Min, Max) :-
 	repeat,
